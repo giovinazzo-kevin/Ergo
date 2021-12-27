246d8335a3cbb6207b8e788338a4cd79015ec936
Kevin Giovinazzo <kevin.giovinazzo@horsa.it>
2021-12-27 11:57:21 +0100

    fix

--- Ergo/Solver/Built-Ins/_Shared/MathBuiltIn.cs
+++ Ergo/Solver/Built-Ins/_Shared/MathBuiltIn.cs
@@ -14,32 +14,32 @@ namespace Ergo.Solver.BuiltIns
             : base(documentation, functor, arity)
         {
         }
-        protected double Eval(ErgoSolver solver, SolverScope scope, ITerm t)
+        protected double Eval(ITerm t)
         {
             if (t is Atom a) { return a.Value is double d ? d : Throw(a); }
             if(t is not Complex c) { Throw(t); }
             return c.Functor switch
             {
                 var f when c.Arguments.Length == 1 && f.Equals(Signature.Functor)
-                => Eval(solver, scope, c.Arguments[0]),
+                => Eval(c.Arguments[0]),
                 var f when c.Arguments.Length == 2 && Operators.BinaryMod.Synonyms.Contains(f) 
-                => Eval(solver, scope, c.Arguments[0]) % Eval(solver, scope, c.Arguments[1])
+                => Eval(c.Arguments[0]) % Eval(c.Arguments[1])
                 , var f when c.Arguments.Length == 2 && Operators.BinarySum.Synonyms.Contains(f) 
-                => Eval(solver, scope, c.Arguments[0]) + Eval(solver, scope, c.Arguments[1])
+                => Eval(c.Arguments[0]) + Eval(c.Arguments[1])
                 , var f when c.Arguments.Length == 2 && Operators.BinarySubtraction.Synonyms.Contains(f) 
-                => Eval(solver, scope, c.Arguments[0]) - Eval(solver, scope, c.Arguments[1])
+                => Eval(c.Arguments[0]) - Eval(c.Arguments[1])
                 , var f when c.Arguments.Length == 2 && Operators.BinaryMultiplication.Synonyms.Contains(f) 
-                => Eval(solver, scope, c.Arguments[0]) * Eval(solver, scope, c.Arguments[1])
+                => Eval(c.Arguments[0]) * Eval(c.Arguments[1])
                 , var f when c.Arguments.Length == 2 && Operators.BinaryDivision.Synonyms.Contains(f) 
-                => Eval(solver, scope, c.Arguments[0]) / Eval(solver, scope, c.Arguments[1])
+                => Eval(c.Arguments[0]) / Eval(c.Arguments[1])
                 , var f when c.Arguments.Length == 2 && Operators.BinaryIntegerDivision.Synonyms.Contains(f) 
-                => (int)(Eval(solver, scope, c.Arguments[0]) / Eval(solver, scope, c.Arguments[1]))
+                => (int)(Eval(c.Arguments[0]) / Eval(c.Arguments[1]))
                 , var f when c.Arguments.Length == 2 && Operators.BinaryPower.Synonyms.Contains(f) 
-                => Math.Pow(Eval(solver, scope, c.Arguments[0]), Eval(solver, scope, c.Arguments[1]))
+                => Math.Pow(Eval(c.Arguments[0]), Eval(c.Arguments[1]))
                 , var f when c.Arguments.Length == 1 && Operators.UnaryNegative.Synonyms.Contains(f) 
-                => -Eval(solver, scope, c.Arguments[0])
+                => -Eval(c.Arguments[0])
                 , var f when c.Arguments.Length == 1 && Operators.UnaryPositive.Synonyms.Contains(f) 
-                => +Eval(solver, scope, c.Arguments[0])
+                => +Eval(c.Arguments[0])
                 , _ => Throw(c)
             };
             static double Throw(ITerm t)

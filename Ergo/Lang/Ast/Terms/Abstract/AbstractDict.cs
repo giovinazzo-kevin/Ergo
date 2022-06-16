02eb9a55c4a1ad47663eadae44686ce3ca98278c
Kevin Giovinazzo <kevin.giovinazzo@horsa.it>
2022-06-16 02:14:56 +0200

    .

--- Ergo/Lang/Ast/Terms/Abstract/AbstractDict.cs
+++ Ergo/Lang/Ast/Terms/Abstract/AbstractDict.cs
@@ -13,18 +13,6 @@ public sealed class Dict : IAbstractTerm
     public readonly ImmutableDictionary<Atom, ITerm> Dictionary;
     public readonly Either<Atom, Variable> Functor;
 
-    public static bool TryUnfold(ITerm term, out Dict dict)
-    {
-        dict = default;
-        if (term is not Complex cplx || !WellKnown.Functors.Dict.Contains(cplx.Functor) || cplx.Arity != 2)
-            return false;
-        var tag = cplx.Arguments[0].Reduce<Either<Atom, Variable>>(a => a, v => v, c => throw new InvalidOperationException());
-        if (!List.TryUnfold(cplx.Arguments[1], out var kvp))
-            return false;
-        dict = new(tag, kvp.Contents.Cast<Complex>().Select(i => new KeyValuePair<Atom, ITerm>((Atom)i.Arguments[0], i.Arguments[1])));
-        return true;
-    }
-
     public Dict(Either<Atom, Variable> functor, IEnumerable<KeyValuePair<Atom, ITerm>> args = default)
     {
         args ??= Enumerable.Empty<KeyValuePair<Atom, ITerm>>();

c002f04efe690cdf0038751b80a10ef3c134512b
Kevin Giovinazzo <kevin.giovinazzo@horsa.it>
2022-06-16 07:04:02 +0200

    fixes

--- Ergo/Lang/Ast/Terms/Abstract/Set.cs
+++ Ergo/Lang/Ast/Terms/Abstract/Set.cs
@@ -7,15 +7,15 @@ public sealed class Set : AbstractList
     public static readonly Set Empty = new(ImmutableArray<ITerm>.Empty);
 
     public Set(ImmutableArray<ITerm> head)
-        : base(head)
+        : base(head.OrderBy(x => x).Distinct())
     {
-        CanonicalForm = Fold(Functor, EmptyElement.WithAbstractForm(Maybe.Some<IAbstractTerm>(Empty)), ImmutableArray.CreateRange(head.OrderBy(x => x).Distinct()))
+        CanonicalForm = Fold(Functor, EmptyElement.WithAbstractForm(Maybe.Some<IAbstractTerm>(Empty)), ImmutableArray.CreateRange(Contents))
             .Reduce<ITerm>(a => a, v => v, c => c)
             .WithAbstractForm(Maybe.Some<IAbstractTerm>(this));
     }
     public Set(IEnumerable<ITerm> contents)
         : this(ImmutableArray.CreateRange(contents)) { }
-    public override Atom Functor => WellKnown.Functors.BracyList.First();
+    public override Atom Functor => WellKnown.Functors.Set.First();
     public override Atom EmptyElement => WellKnown.Literals.EmptyBracyList;
     public override (string Open, string Close) Braces => ("{", "}");
     public override ITerm CanonicalForm { get; }

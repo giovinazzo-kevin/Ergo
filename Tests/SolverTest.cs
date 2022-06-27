3075687c105eef207a079c35c42467f2b1b06777
Kevin Giovinazzo <kevin.giovinazzo@horsa.it>
2022-06-27 16:33:10 +0200

    Fix test exception handler

--- Tests/SolverTest.cs
+++ Tests/SolverTest.cs
@@ -24,13 +24,29 @@ public class SolverTest : ErgoTest
         var facade = ErgoFacade.Standard;
         Interpreter = facade.BuildInterpreter(InterpreterFlags.Default);
         InterpreterScope = Interpreter.CreateScope(x => x
-            .WithExceptionHandler(NullExceptionHandler)
             .WithoutSearchDirectories()
             .WithSearchDirectory(basePath)
             .WithSearchDirectory($@"{basePath}\stdlib\")
             .WithSearchDirectory($@"{basePath}\user\"));
     }
     // "⊤" : "⊥"
+    public async Task ShouldSolve(string query, int numSolutions, params string[] expected)
+    {
+        var testScope = InterpreterScope
+            .WithExceptionHandler(ThrowingExceptionHandler);
+        using var solver = Interpreter.Facade.BuildSolver(testScope.KnowledgeBase, SolverFlags.Default);
+        var parsed = Interpreter.Parse<Query>(testScope, query)
+            .GetOrThrow(new InvalidOperationException());
+        await foreach (var (sol, i) in solver.Solve(parsed, solver.CreateScope(testScope))
+            .Select((x, i) => (x, i)))
+        {
+            Assert.IsTrue(i < expected.Length);
+            Assert.AreEqual(expected[i], sol.Simplify().Substitutions.Join(";"));
+            Assert.IsTrue(--numSolutions >= 0);
+        }
+
+        Assert.AreEqual(0, numSolutions);
+    }
     [DataTestMethod]
     [DataRow("⊥", 0)]
     [DataRow("⊤", 1, "")]
@@ -46,6 +62,9 @@ public class SolverTest : ErgoTest
     [DataRow("(⊤, ⊤); ⊤", 2, "", "")]
     [DataRow("(⊤, ⊤); (⊤, ⊤)", 2, "", "")]
     [DataRow("(⊤, ⊥); (⊥; ⊤)", 1, "")]
+    public Task ShouldSolveConjunctionsAndDisjunctions(string query, int numSolutions, params string[] expected)
+        => ShouldSolve(query, numSolutions, expected);
+    [DataTestMethod]
     [DataRow("!, ⊤", 1, "")]
     [DataRow("⊤, !", 1, "")]
     [DataRow("!, ⊤ ; ⊤", 1, "")]
@@ -55,42 +74,12 @@ public class SolverTest : ErgoTest
     [DataRow("⊤; (⊤, !; ⊤)", 2, "", "")]
     [DataRow("⊤; (⊤; ⊤, !)", 3, "", "", "")]
     [DataRow("(⊤, !; ⊤), !; (!, ⊤; ⊤)", 1, "")]
+    public Task ShouldSolveCuts(string query, int numSolutions, params string[] expected)
+        => ShouldSolve(query, numSolutions, expected);
+    [DataTestMethod]
     [DataRow("setup_call_cleanup(assertz(t:-(⊤)), (t), retractall(t))", 1, "")]
     [DataRow("setup_call_cleanup((assertz(t), assertz(t)), (t), retractall(t))", 2, "", "")]
-    public async Task ShouldSolve(string query, int numSolutions, params string[] expected)
-    {
-        using var solver = Interpreter.Facade.BuildSolver(InterpreterScope.KnowledgeBase, SolverFlags.Default);
-        var parsed = Interpreter.Parse<Query>(InterpreterScope, query)
-            .GetOrThrow(new InvalidOperationException());
-        await foreach (var (sol, i) in solver.Solve(parsed, solver.CreateScope(InterpreterScope))
-            .Select((x, i) => (x, i)))
-        {
-            Assert.IsTrue(i < expected.Length);
-            Assert.AreEqual(expected[i], sol.Simplify().Substitutions.Join(";"));
-            Assert.IsTrue(--numSolutions >= 0);
-        }
+    public Task ShouldSolveSetups(string query, int numSolutions, params string[] expected)
+        => ShouldSolve(query, numSolutions, expected);
 
-        Assert.AreEqual(0, numSolutions);
-    }
-
-    //[DataRow("data(X)", "X/1; X/2")]
-    //[DataRow("data(_)", "; ")]
-    //[DataRow("tuple(X, Y, Z)", "X/1, Y/1, Z/1; X/2, Y/2, Z/2; X/1, Y/2, Z/3")]
-    //[DataRow("indr_3(X)", "X/1; X/2")]
-    //[DataRow("dynamic(X, Y)", "X/1, Y/1; X/1, Y/2; X/2, Y/1; X/2, Y/2")]
-    //[DataRow("dynamic(X, Y, Z)", "X/1, Y/1, Z/1; X/1, Y/1, Z/2; X/1, Y/2, Z/1; X/1, Y/2, Z/2; X/2, Y/1, Z/1; X/2, Y/1, Z/2; X/2, Y/2, Z/1; X/2, Y/2, Z/2")]
-    //[DataRow("yinAndYang([yin], Y)", "Y/[yang]")]
-    //[DataRow("yinAndYang([yin, yin], Y)", "Y/[yang,yang]")]
-    //[DataRow("yinAndYang([yin, yin, yin], Y)", "Y/[yang,yang,yang]")]
-    //[DataRow("map([1,2,3,4,5,6,7,8], X)", "X/[2,3,4,5,6,7,8,9]")]
-    //[DataRow("map([1,2,3], X), map(X, Y)", "X/[2,3,4], Y/[3,4,5]")]
-    //[DataTestMethod]
-    //public void SolveSimpleQuery(string query, string expected)
-    //{
-    //    var (interpreter, scope) = MakeInterpreter();
-    //    var predicates = new Parsed<Query>(query, _ => throw new Exception("Parse fail."), scope.Operators.Value);
-    //    var ans = SolverBuilder.Build(interpreter, ref scope).Solve(predicates.Value.GetOrDefault()).CollectAsync().GetAwaiter().GetResult();
-    //    Assert.IsNotNull(ans);
-    //    Assert.AreEqual(expected, String.Join("; ", ans.Select(e => String.Join(", ", e.Simplify().Substitutions.Select(s => s.Explain())))));
-    //}
 }

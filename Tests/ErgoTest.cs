cb5549823c5d51bef021029c6e4fbabeac7c3a9e
Kevin Giovinazzo <kevin.giovinazzo@horsa.it>
2022-11-15 10:39:26 +0100

    removed old tests

--- Tests/ErgoTest.cs
+++
@@ -1,9 +0,0 @@
-﻿using Ergo.Lang.Exceptions.Handler;
-
-namespace Tests;
-
-public abstract class ErgoTest
-{
-    public static readonly ExceptionHandler NullExceptionHandler = default;
-    public static readonly ExceptionHandler ThrowingExceptionHandler = new(ex => throw ex);
-}
--- Tests/Extensions.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Text.RegularExpressions;
-
-namespace Tests;
-
-public static class Extensions
-{
-    public static string RemoveExtraWhitespace(this string s) => new Regex("\\s+", RegexOptions.Compiled | RegexOptions.Multiline).Replace(s, " ");
-}

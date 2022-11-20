dc6258692b5ab6c5c03702994118b6da8dc9809c
G3Kappa <gkevin@protonmail.com>
2022-11-20 12:59:23 +0100

    fix pull_data/1

--- Ergo/Solver/Built-Ins/CSharp/Pull.cs
+++ Ergo/Solver/Built-Ins/CSharp/Pull.cs
@@ -10,14 +10,14 @@ public sealed class Pull : SolverBuiltIn
     public override IEnumerable<Evaluation> Apply(SolverContext context, SolverScope scope, ITerm[] args)
     {
         var any = false;
-        //await foreach (var item in context.Solver.GetDataSourceMatches(args[0]))
-        //{
-        //    if (item.Rhs.Unify(args[0]).TryGetValue(out var subs))
-        //    {
-        //        any = true;
-        //        yield return new(WellKnown.Literals.True, subs);
-        //    }
-        //}
+        foreach (var item in context.Solver.GetDataSourceMatches(args[0]).ToEnumerable())
+        {
+            if (item.Rhs.Unify(args[0]).TryGetValue(out var subs))
+            {
+                any = true;
+                yield return new(WellKnown.Literals.True, subs);
+            }
+        }
 
         if (!any)
         {

0784db92405edb5caba14f0ddfc1d32149cd1992
G3Kappa <gkevin@protonmail.com>
2023-11-04 17:18:18 +0100

    refactor unify

--- Ergo/Lang/Ast/Unification/Substitution.cs
+++ Ergo/Lang/Ast/Unification/Substitution.cs
@@ -9,11 +9,6 @@ public readonly struct Substitution
     public readonly ITerm Lhs;
     public readonly ITerm Rhs;
 
-    // Set of equality statements
-    private readonly Queue<Substitution> E = new();
-    // Set of substitutions
-    private readonly Queue<Substitution> S = new();
-
     public Substitution(ITerm lhs, ITerm rhs)
     {
         Lhs = lhs;
@@ -37,7 +32,8 @@ public readonly struct Substitution
     public Maybe<SubstitutionMap> Unify()
     {
         var map = new SubstitutionMap();
-        E.Clear();
+        var E = new Queue<Substitution>();
+        var S = new Queue<Substitution>();
         E.Enqueue(this);
         while (E.TryDequeue(out var sub))
         {
@@ -46,68 +42,68 @@ public readonly struct Substitution
         }
         map.AddRange(S);
         return Maybe.Some(map);
-    }
-
-    private void ApplySubstitution(Substitution s)
-    {
-        for (int i = 0; i < E.Count; i++)
+        bool Unify(ITerm x, ITerm y)
         {
-            var eq = E.Dequeue();
-            E.Enqueue(new Substitution(eq.Lhs.Substitute(s), eq.Rhs.Substitute(s)));
-        }
-        for (int i = 0; i < S.Count; i++)
-        {
-            var eq = S.Dequeue();
-            S.Enqueue(new Substitution(eq.Lhs.Substitute(s), eq.Rhs.Substitute(s)));
-        }
-        S.Enqueue(s);
-    }
-
-    private bool Unify(ITerm x, ITerm y)
-    {
-        if (x.Equals(y))
-            return true;
-        if (x.IsGround && y.IsGround)
-            return false;
-        if (y is AbstractTerm ay && ay.Unify(x).TryGetValue(out var ayWithX))
-        {
-            foreach (var s in ayWithX)
-            {
-                if (x is Variable)
-                    ApplySubstitution(new Substitution(x, y));
-                else E.Enqueue(s);
-            }
-            return true;
-        }
-        else if (x is AbstractTerm ax && ax.Unify(y).TryGetValue(out var axWithY))
-        {
-            foreach (var s in axWithY)
-            {
-                if (y is Variable)
-                    ApplySubstitution(new Substitution(y, x));
-                else E.Enqueue(s);
-            }
-            return true;
-        }
-        else if (y is Variable)
-        {
-            ApplySubstitution(new Substitution(y, x));
-            return true;
-        }
-        else if (x is Variable)
-        {
-            ApplySubstitution(new Substitution(x, y));
-            return true;
-        }
-        else if (x is Complex cx && y is Complex cy)
-        {
-            if (!cx.Matches(cy))
+            if (x.Equals(y))
+                return true;
+            if (x.IsGround && y.IsGround)
                 return false;
-            for (var i = 0; i < cx.Arguments.Length; i++)
-                E.Enqueue(new Substitution(cx.Arguments[i], cy.Arguments[i]));
-            return true;
+            if (y is AbstractTerm ay && ay.Unify(x).TryGetValue(out var ayWithX))
+            {
+                foreach (var s in ayWithX)
+                {
+                    if (x is Variable)
+                        ApplySubstitution(new Substitution(x, y));
+                    else E.Enqueue(s);
+                }
+                return true;
+            }
+            else if (x is AbstractTerm ax && ax.Unify(y).TryGetValue(out var axWithY))
+            {
+                foreach (var s in axWithY)
+                {
+                    if (y is Variable)
+                        ApplySubstitution(new Substitution(y, x));
+                    else E.Enqueue(s);
+                }
+                return true;
+            }
+            else if (y is Variable)
+            {
+                ApplySubstitution(new Substitution(y, x));
+                return true;
+            }
+            else if (x is Variable)
+            {
+                ApplySubstitution(new Substitution(x, y));
+                return true;
+            }
+            else if (x is Complex cx && y is Complex cy)
+            {
+                if (!cx.Matches(cy))
+                    return false;
+                for (var i = 0; i < cx.Arguments.Length; i++)
+                    E.Enqueue(new Substitution(cx.Arguments[i], cy.Arguments[i]));
+                return true;
+            }
+            return false;
         }
-        return false;
+
+        void ApplySubstitution(Substitution s)
+        {
+            for (int i = 0; i < E.Count; i++)
+            {
+                var eq = E.Dequeue();
+                E.Enqueue(new Substitution(eq.Lhs.Substitute(s), eq.Rhs.Substitute(s)));
+            }
+            for (int i = 0; i < S.Count; i++)
+            {
+                var eq = S.Dequeue();
+                S.Enqueue(new Substitution(eq.Lhs.Substitute(s), eq.Rhs.Substitute(s)));
+            }
+            S.Enqueue(s);
+        }
+
     }
 
     public override bool Equals(object obj)

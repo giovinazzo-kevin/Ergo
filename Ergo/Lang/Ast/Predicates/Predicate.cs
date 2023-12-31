1e0b9e084e193b31400a5c7b9d951e5a71d6adbf
G3Kappa <gkevin@protonmail.com>
2023-12-31 12:49:00 +0100

    wip

--- Ergo/Lang/Ast/Predicates/Predicate.cs
+++ Ergo/Lang/Ast/Predicates/Predicate.cs
@@ -167,7 +167,7 @@ public readonly struct Predicate : IExplainable
         => new(string.Empty, module, head, NTuple.Empty, dynamic, exported, new ExecutionGraph(WellKnown.Literals.True, TrueNode.Instance));
     public static Predicate Falsehood(Atom module, ITerm head, bool dynamic = false, bool exported = false)
         => new(string.Empty, module, head, NTuple.Empty, dynamic, exported, new ExecutionGraph(WellKnown.Literals.False, FalseNode.Instance));
-    public static Predicate Virtual(Atom module, ITerm head, ErgoVM.Op op, bool dynamic = false, bool exported = false)
+    public static Predicate FromOp(Atom module, ITerm head, ErgoVM.Op op, bool dynamic = false, bool exported = false)
         => new(string.Empty, module, head, NTuple.Empty, dynamic, exported, new ExecutionGraph(WellKnown.Literals.True, new VirtualNode(op)));
 
     public Predicate(string desc, Atom module, ITerm head, NTuple body, bool dynamic, bool exported, bool tailRecursive, Maybe<ExecutionGraph> graph)
